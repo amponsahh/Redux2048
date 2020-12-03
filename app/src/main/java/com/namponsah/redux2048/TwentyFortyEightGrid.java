ffd0be7c20e12c73daa2755f6b0768888366d4e2
amponsah <72049430+namponsah@users.noreply.github.com>
2020-12-03 17:37:46 -0600

    initial setup gameboard class included

---
+++ app/src/main/java/com/namponsah/redux2048/TwentyFortyEightGrid.java
@@ -0,0 +1,18 @@
+/**
+ * 2048 Gameboard Tracker
+ *  This is where the randomization "algorithm", two dimensional array of specific Tile objects
+ *  of each individual tile resides for comparision and manipulations as well the various
+ *  test conditions to move the game board of tile objects
+ *
+ *  Keeps track of the gameboard, tiles[][] and its accompanying methods
+ *  In places where I was stuck, I referenced a Rosetta Code wiki for help
+ *
+ * @author NB Amponsah
+ */
+package com.namponsah.redux2048;
+import java.util.Random;
+import java.util.Vector;
+
+public class TwentyFortyEightGrid {
+
+}
