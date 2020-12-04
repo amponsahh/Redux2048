46bedc0bb01144774080653ca629562d8d4b0c14
amponsah <72049430+namponsah@users.noreply.github.com>
2020-12-03 18:17:02 -0600

    direction enum class for controlling

---
+++ app/src/main/java/com/namponsah/redux2048/Direction.java
@@ -0,0 +1,14 @@
+/**
+ * An enum that serves as a dictionary with a set of
+ *  set of constant values for the
+ *  TwentyFortyEightGrid.java class to determine
+ *  the direction via which the entire grid should move
+ *
+ * @author NB Amponsah
+ */
+package com.namponsah.redux2048;
+
+//https://dzone.com/articles/using-java-enums
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT
+}
